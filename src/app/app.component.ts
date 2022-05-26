994bf52893dc6bee43391adaa535759688998a13
ilopez <ivan.lopez@igmweb.com>
2022-05-26 11:33:33 +0200

    templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'il-astro-bookings';
+  public title = 'Astro Bookings';
+  public subtitle ='Welcome on board';
+  public  author= 'Ivan Lopez';
+  public  authorUrl= 'https://www.marca.com';
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+public reloading=false;
+  public getAgenciesLenght(){
+    return this.agencies.length;
+  }
+
+  public reload(List:String){
+    this.reloading=true;
+    return console.log('Reloading...');
+  }
 }
